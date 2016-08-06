bd304c1050c827921378ed747cf0bf66c8b6d831
danieladeyemo <adeyemodaniel10@gmail.com>
2016-08-06 13:28:59 +0100

    Add couple of tests for countdown procedure

--- assn1/sol_1.scm
+++ assn1/sol_1.scm
@@ -5,13 +5,19 @@
 
 
 (define (join-list list-1 list-2)
-	(cond ((null? list-1) list-2)
-	      (else (cons (car list-1) (join-list (cdr list-1) list-2)))))
-	
+    (cond ((null? list-1) list-2)
+          (else (cons (car list-1) (join-list (cdr list-1) list-2)))))
+    
 (define countdown
-	(lambda (n)
-		(define count
-		  (lambda (n n-list)
-		    (cond ((>= n 0) (count (- n 1) (join-list n-list (list n))))
-		          (else n-list))))
-	(count n '())))
+    (lambda (n)
+        (define count
+          (lambda (n n-list)
+            (cond ((>= n 0) (count (- n 1) (join-list n-list (list n))))
+                  (else n-list))))
+    (count n '())))
+
+;;Tests
+(countdown 6)
+;;>(6 5 4 3 2 0)
+(countdonwn 9)
+;;>(9 8 7 6 5 4 3 2 1 0)
