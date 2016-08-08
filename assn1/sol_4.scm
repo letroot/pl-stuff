071742a03aaf2a133db6e9d0afe7757c5a7914d0
Daniel <adeyemodaniel10@gmail.com>
2016-08-08 17:47:27 +0100

    Buggy sol to problem 4

--- assn1/sol_4.scm
+++ assn1/sol_4.scm
@@ -2,22 +2,26 @@
 ;;the (base 0) index of that element in the list. A list missing that element will be 
 ;;considered bad data.
 
-;;> (list-index-ofv? 'x '(x y z x x))
-;;0
-;;> (list-index-ofv? 'x '(y z x x))
-;;2
+;;Unnatural you are, young Jedi...
+;; (define list-index-ofv?
+;; 	(lambda (sym _list)
+;; 		(define (counter count sym _list)
+;; 			(cond ((null? _list) (- 1))
+;; 			      ((eqv? sym (car _list)) count)
+;; 			      (else (counter (+ 1 count) sym (cdr _list)))))
+;; 	(counter 0 sym _list)))
 
+;;Solution
+;;I wonder why this doesn't work for elements at index 0
 (define list-index-ofv?
 	(lambda (sym _list)
-		(define (counter count sym _list)
-			(cond ((null? _list) (- 1))
-			      ((eqv? sym (car _list)) count)
-			      (else (counter (+ 1 count) sym (cdr _list)))))
-	(counter 0 sym _list)))
+			(cond ((null? _list) nil)
+			      ((eqv? sym (car _list)) 0)
+			      (else (+ 1 (list-index-ofv? sym (cdr _list)))))))
 
 ;;Tests
 (list-index-ofv? 'z '(y z x x))
 ;;1
 
-(list-index-ofv? '1 '(4 2 5 2 1 9))
-;;4
+(list-index-ofv? '4 '(4 2 5 2 1 9))
+;;returns nothing!?
