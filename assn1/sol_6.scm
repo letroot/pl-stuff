7e60c9ced936f376ea9f14fbf41d32ab7b8b39ad
Daniel <adeyemodaniel10@gmail.com>
2016-08-09 11:03:44 +0100

    Write naturally recursive solution to problem 6

--- assn1/sol_6.scm
+++ assn1/sol_6.scm
@@ -2,22 +2,11 @@
 ;;formed by combining the corresponding elements of the two input lists. If the two lists are of uneven length, 
 ;;then drop the tail of the longer one.
 
-;;I already defined join-list in the solution to problem 1 :D
-; (define join-list
-;   (lambda (list-1 list-2)
-;     (cond ((null? list-1) list-2)
-;           (else (cons (car list-1) (join-list (cdr list-1) list-2))))))
-
-; (define zip
-;   (lambda (list-1 list-2)
-;     (define zipper
-;       (lambda (list-1 list-2 new)
-;         (if (or (null? list-1) (null? list-2))
-;             new
-;             (zipper (cdr list-1) (cdr list-2) (join-list new (list (cons (car list-1) (car list-2))))))))
-;     (zipper list-1 list-2 '())))
-
-;; Tomorrow, play around with (cons (cons (car l1) (car l2)) (list (zip (cdr l1) (cdr l2))))
+(define zip
+  (lambda (list-1 list-2)
+    (cond
+      [(or (null? list-1) (null? list-2)) '()]
+      [else (cons (cons (car list-1) (car list-2)) (zip (cdr list-1) (cdr list-2)))])))
 
 ;;Tests
 (zip '(1 2 3) '(2 3 4))
