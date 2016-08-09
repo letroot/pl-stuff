4117376082a71730f002a9e13eb345ee0592976e
Daniel <adeyemodaniel10@gmail.com>
2016-08-09 12:57:59 +0100

    Fix infinite loop

--- assn1/sol_2.scm
+++ assn1/sol_2.scm
@@ -2,20 +2,17 @@
 ;;symbol inserted after each occurrence of the first symbol. For this and later questions, these functions need 
 ;;only hold over eqv?-comparable structures.
 
-;;> (insertR 'x 'y '(x z z x y x))
-;;(x y z z x y y x y)
-
 (define (insertR sym-1 sym-2 l) 
-  (cond
-    [(null? l) (list)]
-    [(eqv? sym-1 (car l)) 
-      (cons sym-1 (insertR sym-1 sym-2 (cons sym-2 (cdr l))))]
-    [else 
-      (cons (car l) (insertR sym-1 sym-2 (cdr l)))]))
+  (cond 
+    ((null? l) '())
+    ((eqv? sym-1 (car l)) 
+        (cons sym-1 (cons sym-2 (insertR sym-1 sym-2 (cdr l)))))
+    (else 
+        (cons (car l) (insertR sym-1 sym-2 (cdr l))))))
 	          
 ;;Tests
 (insertR 'x 'y '(x x y g d s x))
 ;;(x y x y y g d s x y)
 
-(insertR 'x 'y '(c d x x y g d s x))
-;;(c d x y x y y g d s x y)
+(insertR 'x 'x '(x))
+;;(x x)
