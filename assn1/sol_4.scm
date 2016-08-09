0f07f36f914f4e4a229273fe1269bf88003175a7
Daniel <adeyemodaniel10@gmail.com>
2016-08-09 12:27:30 +0100

    Correct and naturally recursive solution

--- assn1/sol_4.scm
+++ assn1/sol_4.scm
@@ -2,16 +2,6 @@
 ;;the (base 0) index of that element in the list. A list missing that element will be 
 ;;considered bad data.
 
-;; (define list-index-ofv?
-;; 	(lambda (sym _list)
-;; 		(define (counter count sym _list)
-;; 			(cond ((null? _list) (- 1))
-;; 			      ((eqv? sym (car _list)) count)
-;; 			      (else (counter (+ 1 count) sym (cdr _list)))))
-;; 	(counter 0 sym _list)))
-
-;;Solution
-;;I wonder why this doesn't work for elements at index 0
 (define list-index-ofv?
   (lambda (sym _list)
     (cond 
@@ -24,4 +14,4 @@
 ;;1
 
 (list-index-ofv? '4 '(4 2 5 2 1 9))
-;;returns nothing!?
+;;0
