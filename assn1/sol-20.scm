f64887c6f6ede724ebe70a48901cdd906160479b
Daniel <adeyemodaniel10@gmail.com>
2016-08-13 22:11:18 +0100

    Add test

--- assn1/sol-20.scm
+++ assn1/sol-20.scm
@@ -1,7 +1,7 @@
-The cartesian-product is defined over a list of sets (again simply lists that by our agreed
-upon convention don't have duplicates). The result is a list of tuples (i.e. lists). Each
-tuple has in the first position an element of the first set, in the second position an element
-of the second set, etc. The output list should contains all such combinations.
+;;The cartesian-product is defined over a list of sets (again simply lists that by our agreed
+;;upon convention don't have duplicates). The result is a list of tuples (i.e. lists). Each
+;;tuple has in the first position an element of the first set, in the second position an element
+;;of the second set, etc. The output list should contains all such combinations.
 
 (define cartesian-product
   (lambda (ls1 ls2)
@@ -14,3 +14,8 @@ of the second set, etc. The output list should contains all such combinations.
     (cond
       [(null? f-ls) '()]
       [else (append (list (cons first (car f-ls))) (ramen first (cdr f-ls)))])))
+
+
+;;Tests
+(cartesian-product '((5 4) (3 2 1)))
+;;((5 3) (5 2) (5 1) (4 3) (4 2) (4 1))
